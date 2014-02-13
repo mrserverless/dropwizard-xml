8019da6c1e7f9eedf88c23c5173c36f1b3cf09d3
Yun Zhi Lin <yun.zhi.lin@gmail.com>
2014-02-13 17:44:36 +1100

    updating test case

--- dropwizard-jackson-xml/src/test/java/com/yunspace/dropwizard/jersey/jackson/xml/UnwrappedAttributeTest.java
+++ dropwizard-jackson-xml/src/test/java/com/yunspace/dropwizard/jersey/jackson/xml/UnwrappedAttributeTest.java
@@ -1,19 +1,15 @@
 package com.yunspace.dropwizard.jersey.jackson.xml;
 
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
-import com.sun.jersey.core.util.MultivaluedMapImpl;
-import com.sun.jersey.core.util.StringKeyObjectValueIgnoreCaseMultivaluedMap;
-import com.yunspace.dropwizard.jackson.xml.JacksonXML;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
+import org.junit.Before;
 import org.junit.Test;
 
-import javax.validation.Validation;
-import javax.ws.rs.core.MediaType;
-import javax.xml.bind.annotation.XmlElement;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.fest.assertions.api.Assertions.assertThat;
@@ -26,64 +22,72 @@ import static org.fest.assertions.api.Assertions.assertThat;
  */
 
 public class UnwrappedAttributeTest {
-    private static final Annotation[] NONE = new Annotation[0];
 
-    //@XmlAccessorType(XmlAccessType.FIELD)
     @JacksonXmlRootElement(localName = "root")
-    //@XmlRootElement(name = "root")
-    static class Root {
+    public class Root {
 
-//        @JacksonXmlProperty(localName = "unwrapped")
-//        @JacksonXmlElementWrapper(useWrapping = false)
-        @XmlElement(name = "unwrapped")
+        public Root() {}
+
+        @JacksonXmlProperty(localName = "unwrapped")
+        @JacksonXmlElementWrapper(useWrapping = false)
         public List<UnwrappedElement> unwrapped;
 
         @JacksonXmlProperty(localName = "name")
         public String name;
 
-        public static class UnwrappedElement {
-            @JacksonXmlProperty(isAttribute = true)
-            public String id;
-
-            @JacksonXmlProperty(isAttribute = true)
-            public String type;
-        }
     }
 
-    protected final static XmlMapper xmlMapper = JacksonXML.newXMLMapper();
-    protected final static JacksonXMLMessageBodyProvider provider = new JacksonXMLMessageBodyProvider(xmlMapper, Validation.buildDefaultValidatorFactory().getValidator());
-    protected final String rootXml = "<root><unwrapped id=\"1\" type=\"string\"/><unwrapped id=\"2\" type=\"string\"/><name>text</name></root>";
+    @JacksonXmlRootElement(localName = "unwrapped")
+    public class UnwrappedElement {
+
+        public UnwrappedElement() {
+
+        }
+
+        public UnwrappedElement (String id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        @JacksonXmlProperty(isAttribute = true, localName = "id")
+        public String id;
+
+        @JacksonXmlProperty(isAttribute = true, localName = "type")
+        public String name;
+    }
+
+    protected final XmlMapper xmlMapper = new XmlMapper();
+    protected final String rootXml =
+            "<root>"  + System.lineSeparator() +
+            "  <unwrapped id=\"1\" name=\"string\"/>"  + System.lineSeparator() +
+            "  <unwrapped id=\"2\" name=\"string\"/>"  + System.lineSeparator() +
+            "  <name>text</name>"  + System.lineSeparator() +
+            "</root>";
+
     protected final Root rootObject = new Root();
 
+    @Before
+    public void setUp() {
+        xmlMapper.enable(SerializationFeature.INDENT_OUTPUT).registerModule(new JaxbAnnotationModule());
 
-    @Test
-    public void deserializeTest () throws Exception {
-        final ByteArrayInputStream requestList = new ByteArrayInputStream(rootXml.getBytes());
-        final Class<?> klass = Root.class;
-        final Object obj = provider.readFrom((Class<Object>) klass,
-                Root.class,
-                NONE,
-                MediaType.APPLICATION_XML_TYPE,
-                new MultivaluedMapImpl(),
-                requestList);
-
+        rootObject.unwrapped = Arrays.asList(
+                new UnwrappedElement("1", "string"),
+                new UnwrappedElement("2", "string")
+        );
+        rootObject.name = "text";
     }
 
     @Test
     public void serializeTest () throws Exception {
-        Root root = xmlMapper.readValue(rootXml, Root.class);
-        final ByteArrayOutputStream output = new ByteArrayOutputStream();
-        final Class<?> klass = Root.class;
-
-        provider.writeTo(rootObject,
-                klass,
-                klass,
-                NONE,
-                MediaType.APPLICATION_XML_TYPE,
-                new StringKeyObjectValueIgnoreCaseMultivaluedMap(),
-                output);
-
-        assertThat(root).isEqualsToByComparingFields(rootObject);
+        assertThat(xmlMapper.writeValueAsString(rootObject)).isEqualTo(rootXml);
     }
 
+    @Test
+    public void derializeTest () throws Exception {
+
+        Root rootResult = (Root) xmlMapper.readValue(rootXml, Root.class);
+
+        assertThat(rootResult).isEqualsToByComparingFields(rootObject);
+    }
 }
+
